6b4d91cd69d18bc17a4c6b2dda412e6d5d846d54
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 02:17:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,8 +6,19 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-const input = document.querySelector('input');
+let input = document.querySelector('input');
  
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
+});
+
+input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
 });
